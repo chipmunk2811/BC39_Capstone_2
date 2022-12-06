08c98110127919fa0c23bd3d863d8fd0061d8c84
chipmunk2811 <114871215+chipmunk2811@users.noreply.github.com>
2022-12-07 02:20:26 +0700

    Delete js directory

--- js/databaseAPI/getListAPI.js
+++
@@ -1,9 +0,0 @@
-class GetAPI{
-    getListAPI(){
-        return axios({
-            url: "https://637b69a46f4024eac20ce266.mockapi.io/api/Capstone",
-            method: "GET",
-        })
-    };
-}
-
